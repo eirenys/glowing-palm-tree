775a963631aa579ac81e10b3b44fe01075cae022
Leonid Timoshin <eirenys@gmail.com>
2017-08-22 03:06:21 +0300

    entities 2

--- src/main/kotlin/io/highload/scheme/location.kt
+++ src/main/kotlin/io/highload/scheme/location.kt
@@ -8,9 +8,57 @@ class Locations {
 }
 
 class Location {
+    private var fields = 0
+
     var id: Int = 0
+        set(value) {
+            fields = fields or 1
+            field = value
+        }
+
     var place: String = ""
+        set(value) {
+            fields = fields or 2
+            field = value
+        }
+
     var country: String = ""
+        set(value) {
+            fields = fields or 4
+            field = value
+        }
+
     var city: String = ""
+        set(value) {
+            fields = fields or 8
+            field = value
+        }
+
     var distance: Int = 0
+        set(value) {
+            fields = fields or 16
+            field = value
+        }
+
+    fun checkEntity() {
+        check(fields == ALL)
+    }
+
+    fun modify(other: Location) {
+        other.ifHaveValue(1) { id = other.id}
+        other.ifHaveValue(2) { place = other.place}
+        other.ifHaveValue(4) { country = other.country}
+        other.ifHaveValue(8) { city = other.city}
+        other.ifHaveValue(16) { distance = other.distance}
+    }
+
+    private inline fun ifHaveValue(flag: Int, block: () -> Unit) {
+        if ((fields and flag) == flag) {
+            block()
+        }
+    }
+
+    companion object {
+        private const val ALL = 0x1F
+    }
 }
--- src/main/kotlin/io/highload/scheme/visit.kt
+++ src/main/kotlin/io/highload/scheme/visit.kt
@@ -10,9 +10,58 @@ class Visits {
 }
 
 class Visit {
+    private var fields = 0
+
     var id: Int = 0
+        set(value) {
+            fields = fields or 1
+            field = value
+        }
+
     var location: Int = 0
+        set(value) {
+            fields = fields or 2
+            field = value
+        }
+
     var user: Int = 0
-    @JsonProperty("visited_at") var visitedAt: Long = 0
+        set(value) {
+            fields = fields or 4
+            field = value
+        }
+
+    @JsonProperty("visited_at")
+    var visitedAt: Long = 0
+        set(value) {
+            fields = fields or 8
+            field = value
+        }
+
     var mark: Int = 0
+        set(value) {
+            fields = fields or 16
+            field = value
+        }
+
+    fun checkEntity() {
+        check(fields == ALL)
+    }
+
+    fun modify(other: Visit) {
+        other.ifHaveValue(1) { id = other.id}
+        other.ifHaveValue(2) { location = other.location}
+        other.ifHaveValue(4) { user = other.user}
+        other.ifHaveValue(8) { visitedAt = other.visitedAt}
+        other.ifHaveValue(16) { mark = other.mark}
+    }
+
+    private inline fun ifHaveValue(flag: Int, block: () -> Unit) {
+        if ((fields and flag) == flag) {
+            block()
+        }
+    }
+
+    companion object {
+        private const val ALL = 0x1F
+    }
 }
