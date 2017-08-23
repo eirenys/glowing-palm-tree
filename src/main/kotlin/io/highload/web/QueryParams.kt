8979fefb8ba245b98d8efce67d87e0a856cd7b14
Leonid Timoshin <eirenys@gmail.com>
2017-08-23 19:08:43 +0300

    query parameters

---
+++ src/main/kotlin/io/highload/web/QueryParams.kt
@@ -0,0 +1,43 @@
+package io.highload.web
+
+import org.joda.time.DateTime
+import java.net.URLDecoder
+
+/**
+ *
+ */
+class QueryParams(
+        val country: String?,
+        val fromDate: Int?,
+        var toDate: Int?,
+        val toDistance: Int?,
+        var fromBirth: Int?,
+        var toBirth: Int?,
+        val gender: Char?) {
+    companion object {
+        fun parse(query: CharSequence): QueryParams {
+            var country: String? = null
+            var fromDate: Int? = null
+            var toDate: Int? = null
+            var toDistance: Int? = null
+            var fromBirth: Int? = null
+            var toBirth: Int? = null
+            var gender: Char? = null
+
+            query.split("&").forEach{
+                val pair = it.split("=")
+                when(pair[0]) {
+                    "country" -> country = URLDecoder.decode(pair[1], "UTF-8")
+                    "fromDate" -> fromDate = pair[1].toInt()
+                    "toDate" -> toDate = pair[1].toInt()
+                    "toDistance" -> toDistance = pair[1].toInt()
+                    "fromAge" -> toBirth = (DateTime.now().minusYears(pair[1].toInt()).millis / 1000).toInt()
+                    "toAge" -> fromBirth = (DateTime.now().minusYears(pair[1].toInt()).millis / 1000).toInt()
+                    "gender" -> gender = pair[1].first()
+                }
+            }
+
+            return QueryParams(country, fromDate, toDate, toDistance, fromBirth, toBirth, gender)
+        }
+    }
+}
---
+++ src/test/kotlin/io/highload/web/QueryParamsTest.kt
@@ -0,0 +1,21 @@
+package io.highload.web
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ *
+ */
+class QueryParamsTest {
+    @Test
+    fun parametersTest() {
+        val query = QueryParams.parse("country=test&toDate=1")
+        assertEquals("test", query.country)
+        assertEquals(1, query.toDate)
+    }
+    @Test
+    fun countryTest() {
+        val query = QueryParams.parse("country=%D0%98%D0%BE%D1%80%D0%B4%D0%B0%D0%BD%D0%B8%D1%8F")
+        assertEquals("Иордания", query.country)
+    }
+}
