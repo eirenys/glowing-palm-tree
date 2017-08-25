c45a77781c9c5c95dde7c90c1b0dad4d67c7fc3f
Leonid Timoshin <eirenys@gmail.com>
2017-08-25 17:02:10 +0300

    http constants

---
+++ src/main/kotlin/io/highload/web/constants.kt
@@ -0,0 +1,12 @@
+package io.highload.web
+
+/**
+ *
+ */
+val HTTP_OK = "HTTP/1.1 200 OK\r\n".toByteArray()
+val HTTP_ERROR = "HTTP/1.1 400 Error\r\n".toByteArray()
+val HTTP_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n".toByteArray()
+val CONTENT_TYPE_JSON = "Content-Type: application/json;charset=utf-8\r\n".toByteArray()
+val CONTENT_LENGTH = "Content-Length: ".toByteArray()
+val LINE = "\r\n".toByteArray()
+val LINELINE = "\r\n\r\n".toByteArray()
